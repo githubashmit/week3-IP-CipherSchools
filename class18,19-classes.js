904acedd847188a29026d4cfbc1a5134615fad57
Ashmit <ashmitgithub@gmail.com>
2022-12-26 18:15:08 +0530

    commit10

--- class18,19-classes.js
+++
@@ -1,7 +0,0 @@
-import pencilBox from "./class18,19.js";
-const newPencilBox = new pencilBox("pencil Box",30 ,"red",12,12,24,false,
-"2020-05-1 00:00:00")
-console.log("The Pencil Box Object:", newPencilBox);
-consoke.log("size:", newPencilBox.size);
-console.log("Date Purchased:", newPencilBox.datePurchased);
-console.log("Date Purchased:", newPencilBox.pencilBoxAge());
