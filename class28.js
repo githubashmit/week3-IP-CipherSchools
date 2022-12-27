baef41d50eb850406a3f171bf64567f8a8b04b88
Ashmit <ashmitgithub@gmail.com>
2022-12-27 17:31:34 +0530

    commit16

--- class28.js
+++ class28.js
@@ -18,7 +18,7 @@ const content = `
             <li>Zip Length Left:  ${newPencilBox.zipLength.left}</li>
             <li>Zip Length Right:  ${newPencilBox.zipLength.right}</li>
             <li>Zip Length Top:  ${newPencilBox.zipLength.top}</li>
-            <li>Zip Status:  ${newPencilBox.zipOpen}</li>
+            <li>Zip Status:  ${newPencilBox.zipOpen ? "The zip is open":"The zip is closed"}</li>
         </ul>
     </main>
 `;
@@ -28,3 +28,39 @@ console.log("size:", newPencilBox.size);
 console.log("Date Purchased:", newPencilBox.datePurchased);
 console.log("Date Purchased:", newPencilBox.pencilBoxAge());
 
+if(pencilBox.zipOpen===true){
+    console.log("Open")
+}else{
+    console.log("Close")
+}
+if(pencilBox.zipOpen===true && newPencilBox===30){
+    console.log("yes")
+}else{
+    console.log("No")
+}
+if(pencilBox.zipOpen===true || newPencilBox===30){
+    console.log("yes")
+}else{
+    console.log("No")
+}
+if(pencilBox.zipOpen===false || newPencilBox===30){
+    console.log("yes")
+}else{
+    console.log("No")
+}
+
+// switch case
+let size=30;
+let boxSize;
+switch(true){
+    case size>30:
+        boxSize="large";
+        break;
+        case size<=30 && size >=20:
+            boxSize="medium";
+            break;
+        case size<=19 && size >=10:
+            boxSize="small";
+}
+console.log("Box Size:",size);
+console.log("Status:",boxSize);
